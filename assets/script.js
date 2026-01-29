c0e6de31c5ce0fe054e1262a051d6ec5a4abd7ba
sbourbousse <sbourbousse@gmail.com>
2026-01-29 14:21:33 +0100

    correction images

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,28 @@
+let currentSlide = 0;
+
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+const dotsContainer = document.querySelector('.dots')
+const arrowLeft = document.querySelector('.arrow_left');// cherhche le premier element qui a la classe arrow_left
+const arrowRight = document.querySelector('.arrow_right');
+
 function updateSlider() {
 	const bannerImage = document.querySelector('.banner-img');
 	const bannerTagLine = document.querySelector('.banner-tagline');
@@ -32,28 +57,6 @@ function updateSlider() {
 	}
 }
 
-let currentSlide = 0;
-
-const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
-const arrowLeft = document.querySelector('.arrow_left');// cherhche le premier element qui a la classe arrow_left
-const arrowRight = document.querySelector('.arrow_right');
 // event listener pour les fleches cest une fonction qui attend un evenement ( la on attend le clic )
 
 arrowRight.addEventListener('click', () => {
@@ -66,8 +69,6 @@ arrowRight.addEventListener('click', () => {
 	updateSlider();
   });
 
-
-const dotsContainer = document.querySelector('.dots')
 function createDots(){
 for (let i = 0; i < slides.length; i++) {
 
